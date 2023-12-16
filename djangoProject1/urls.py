25d85677979aade957236b080100ebff7431ca86
Uli <murataalievu@gmail.com>
2023-12-16 22:43:07 +0600

    Установил все необходимое

---
+++ djangoProject1/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+urlpatterns += static
---
+++ neobis_challenge/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+    image = models.ImageField(upload_to='category')
+
+    def __str__(self):
+        return self.image
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=100, blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+
+    def __str__(self):
+        return self.title
